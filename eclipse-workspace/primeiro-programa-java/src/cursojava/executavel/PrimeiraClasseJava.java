0d51667123c0a86d9ca0753a4ac9da1841ef7b73
CHIP7 <CHIP7@DESKTOP-995CF5N>
2023-07-25 11:28:38 -0300

    primeiro programa java

---
+++ eclipse-workspace/primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,27 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	
+	/*Main é um metodo auto executavel em java*/
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		int num2 = 2;
+		int soma= num1+num2;
+		int subtracao = num1-num2;
+		int multiplicacao = num1*num2;
+		int divisao = num1/num2;
+		
+		
+		
+		/*Escrever mensagem*/
+		System.out.println("A Soma de:"+num1 +" + "+num2 +" é igual á:"+ soma);
+		System.out.println("A Subtracão de:"+num1 +" - "+num2 +" é igual á:"+ subtracao);
+		System.out.println("A Multiplicação de:"+num1 +" * "+num2 +" é igual á:"+ multiplicacao);
+		System.out.println("A Divisão de:"+num1 +" / "+num2 +" é igual á:"+ divisao);
+		
+		
+	}
+
+}
