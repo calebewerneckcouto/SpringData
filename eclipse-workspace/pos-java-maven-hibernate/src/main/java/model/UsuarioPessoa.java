967d82cbc89665619ecdd86808a790cebe44364c
CHIP7 <CHIP7@DESKTOP-995CF5N>
2023-07-26 13:12:24 -0300

    atualizando banco de dados

--- eclipse-workspace/pos-java-maven-hibernate/src/main/java/model/UsuarioPessoa.java
+++ eclipse-workspace/pos-java-maven-hibernate/src/main/java/model/UsuarioPessoa.java
@@ -16,6 +16,23 @@ public class UsuarioPessoa{
 	private String email;
 	private String login;
 	private String senha;
+	private int idade;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 	public Long getId() {
 		return id;
 	}
