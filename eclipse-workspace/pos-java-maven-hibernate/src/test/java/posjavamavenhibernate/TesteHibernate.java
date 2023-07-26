bbec4d63ac46698a4106321de992988840fa6ada
CHIP7 <CHIP7@DESKTOP-995CF5N>
2023-07-26 13:20:56 -0300

    salvando dados com hibernate e maven

--- eclipse-workspace/pos-java-maven-hibernate/src/test/java/posjavamavenhibernate/TesteHibernate.java
+++ eclipse-workspace/pos-java-maven-hibernate/src/test/java/posjavamavenhibernate/TesteHibernate.java
@@ -2,10 +2,25 @@ package posjavamavenhibernate;
 
 import org.junit.Test;
 
+import dao.DaoGeneric;
+import model.UsuarioPessoa;
+
 public class TesteHibernate {
 	@Test
 	public void testeHibernate() {
-		HibernateUtil.getEntityManager();
+		DaoGeneric<UsuarioPessoa> daoGeneric = new  DaoGeneric<>();
+		
+		UsuarioPessoa pessoa = new UsuarioPessoa();
+		
+		pessoa.setIdade(35);
+		pessoa.setLogin("cwc3d");
+		pessoa.setNome("Calebe Werneck Couto");
+		pessoa.setEmail("calebewerneck@hotmail.com");
+		pessoa.setSobrenome("Werneck Couto");
+		pessoa.setSenha("cwc3d14694899");
+		
+		daoGeneric.salvar(pessoa);
+		
 	}
 
 }
