b900e0d3efe8793e8dfddab560a26a3d64378886
CHIP7 <CHIP7@DESKTOP-995CF5N>
2023-07-27 16:28:49 -0300

    Usando binding para controle itens no frontend atraves do backend

--- eclipse-workspace/meuprimeiroprojetojsf/src/main/java/br/com/cursojsf/PessoaBean.java
+++ eclipse-workspace/meuprimeiroprojetojsf/src/main/java/br/com/cursojsf/PessoaBean.java
@@ -7,14 +7,29 @@ import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
 
 @ManagedBean(name = "pessoaBean")
-@ApplicationScoped
+@ViewScoped
 public class PessoaBean {
 
 	private String nome;
+	
+	private HtmlCommandButton  commandButton;
 
 	private List<String> nomes = new ArrayList<>();
+	
+	
+	
+	
+
+	public HtmlCommandButton getCommandButton() {
+		return commandButton;
+	}
+
+	public void setCommandButton(HtmlCommandButton commandButton) {
+		this.commandButton = commandButton;
+	}
 
 	public List<String> getNomes() {
 		return nomes;
@@ -25,7 +40,15 @@ public class PessoaBean {
 	}
 
 	public String addNome() {
+		 
+		
 		nomes.add(nome);
+		
+		if(nomes.size()>5) {
+			commandButton.setDisabled(true);
+		}
+		
+		
 		return "";
 	}
 
