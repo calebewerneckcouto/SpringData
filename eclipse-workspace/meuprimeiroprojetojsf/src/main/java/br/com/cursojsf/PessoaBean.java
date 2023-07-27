8c2d9cae3d42bff3e544f32508ebce2a293849ac
CHIP7 <CHIP7@DESKTOP-995CF5N>
2023-07-27 16:22:29 -0300

    @ApplicationScoped

--- eclipse-workspace/meuprimeiroprojetojsf/src/main/java/br/com/cursojsf/PessoaBean.java
+++ eclipse-workspace/meuprimeiroprojetojsf/src/main/java/br/com/cursojsf/PessoaBean.java
@@ -3,12 +3,13 @@ package br.com.cursojsf;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 
 @ManagedBean(name = "pessoaBean")
-@SessionScoped
+@ApplicationScoped
 public class PessoaBean {
 
 	private String nome;
