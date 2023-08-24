c03b644a0510505a363505453f76013eaf4e10b4
CHIP7 <CHIP7@DESKTOP-995CF5N>
2023-08-24 14:47:15 -0300

    Novo Projeto Hibernate e Spring

---
+++ eclipse-workspace/cwc3d/src/main/java/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -0,0 +1,8 @@
+package br.com.framework.implementacao.crud;
+
+/*Nome do Caminho do JNDI datasource TomCat*/
+public class VariavelConexaoUtil {
+	
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+
+}
