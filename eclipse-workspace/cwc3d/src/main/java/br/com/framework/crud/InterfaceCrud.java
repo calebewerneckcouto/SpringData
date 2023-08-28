7a3abd0a26b6b59d12079add137c93b03181f849
CHIP7 <CHIP7@DESKTOP-995CF5N>
2023-08-28 10:45:38 -0300

    Atualizando

--- eclipse-workspace/cwc3d/src/main/java/br/com/framework/crud/InterfaceCrud.java
+++ eclipse-workspace/cwc3d/src/main/java/br/com/framework/crud/InterfaceCrud.java
@@ -3,9 +3,8 @@ package br.com.framework.crud;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.mail.Session;
-
 import org.hibernate.Query;
+import org.hibernate.Session;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
@@ -15,78 +14,88 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 @Transactional
 public interface InterfaceCrud<T> extends Serializable {
-	/*
-	 * Usa Metodo Generico para salvar qualquer objeto
-	 * 
-	 * Representado por <T>
-	 * 
-	 */
 
-	/* Salva os dados */
+	T findMaxObjectEntity(Class<T> classObj) throws Exception;
+
 	void save(T obj) throws Exception;
+
+	void clearSession() throws Exception;
+
+	T saveRetorno(T obj) throws Exception;
 
 	void persist(T obj) throws Exception;
 
-	/* Salva ou Atualiza */
-	void saveOrUpdate(T obj) throws Exception;
+	void saveOrUpdate(Object obj) throws Exception;
 
-	// Realiza o Update/atualização dos dados
 	void update(T obj) throws Exception;
 
-	// Realiza o Delete dos dados
 	void delete(T obj) throws Exception;
 
-	// Salva ou atualiza e retorna o objeto em estado persistente
 	T merge(T obj) throws Exception;
 
-	// Carrega a lista de dadis de determinada classe.
-	List<T> findList(Class<T> objs) throws Exception;
+	List<T> finList(Class<T> obj) throws Exception;
 
 	Object findById(Class<T> entidade, Long id) throws Exception;
 
-	T findByPorId(Class<T> entidade, Long id) throws Exception;
+	List<T> findListByProperty(Class<T> entidade, Object atributo, Object valor)
+			throws Exception;
+
+	List<T> findListByListDeIds(Class<T> obj, List<Long> cods) throws Exception;
 
 	List<T> findListByQueryDinamica(String s) throws Exception;
 
-	// executar update com HQL( hibernate)
 	void executeUpdateQueryDinamica(String s) throws Exception;
 
-	// Executar update com SQL
 	void executeUpdateSQLDinamica(String s) throws Exception;
 
-	// limpa a sessão do Hibernate
-	void clearSession() throws Exception;
+	void evict(Object obj) throws Exception;
 
-	// Retira o Objeto da Sessão do Hibernate
-	void evict(Object objs) throws Exception;
+	T carregar(Class<T> class1, Long long1) throws Exception;
+
+	Class<T> getClass(Class<T> entidade) throws Exception;
+
+	List<T> findListByLike(Class<T> entidade, String atributoClass, String valor)
+			throws Exception;
+
+	List<T> findByPropertyId(Class<T> entidade, Long id, Object atributo)
+			throws Exception;
 
 	Session getSession() throws Exception;
 
 	List<?> getListSQLDinamica(String sql) throws Exception;
 
-	T findUninqueByPropertyId(Class<T> entidade, Long id, Object atributo) throws Exception;
+	T findUninqueByPropertyId(Class<T> entidade, Long id, Object atributo)
+			throws Exception;
 
 	T findUniqueByQueryDinamica(String query) throws Exception;
 
 	void executeUpdateSQLDinamica(String query, Long... cods) throws Exception;
 
-	List<T> finListOrderByProperty(Class<T> entidade, String propriedade) throws Exception;
+	List<T> finListOrderByProperty(Class<T> entidade, String propriedade)
+			throws Exception;
 
-	T findUninqueByProperty(Class<T> entidade, Object valor, String atributo) throws Exception;
+	T findUninqueByProperty(Class<T> entidade, Object valor, String atributo)
+			throws Exception;
 
-	T findUninqueByPropertyId(Class<T> entidade, Long id, Object atributo, String condicaoAdicional) throws Exception;
+	T findUninqueByPropertyId(Class<T> entidade, Long id, Object atributo,
+			String condicaoAdicional) throws Exception;
 
 	JdbcTemplate getJdbcTemplate();
 
 	SimpleJdbcTemplate getSimpleJdbcTemplate();
 
 	SimpleJdbcInsert getSimpleJdbcInsert();
-
-	public T findUninqueByProperty(Class<T> entidade, Object valor, String atributo, String condicao) throws Exception;
-
+	
+	public T findUninqueByProperty(Class<T> entidade, Object valor,
+			String atributo, String condicao) throws Exception;
+	
 	Long totalRegistro(String tabela) throws Exception;
-
+	
 	Query obterQuery(String query) throws Exception;
+	
+	 List<T> findListByQueryDinamica(String query, int iniciaNoRegistro, int maximoResultado) throws Exception;
 
-	List<T> findListByQueryDinamica(String query, int iniciaNoRegistro, int maximoResultado) throws Exception;
+	List<T> findList(Class<T> objs) throws Exception;
+
+	T findByPorId(Class<T> entidade, Long id) throws Exception; 
 }
--- eclipse-workspace/cwc3d/src/main/java/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
+++ eclipse-workspace/cwc3d/src/main/java/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -1,8 +1,12 @@
 package br.com.framework.implementacao.crud;
 
-/*Nome do Caminho do JNDI datasource TomCat*/
+/**
+ * Armazena variaveis de data source
+ * 
+ *
+ */
 public class VariavelConexaoUtil {
-	
-	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+
+	public static final String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasouce";
 
 }
