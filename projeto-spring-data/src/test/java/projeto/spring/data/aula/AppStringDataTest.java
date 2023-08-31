24f65868dfb0e4dd2aaa8bcd8f299af0ec44b094
CHIP7 <CHIP7@DESKTOP-995CF5N>
2023-08-31 09:08:27 -0300

    Consultar Todos...

--- projeto-spring-data/src/test/java/projeto/spring/data/aula/AppStringDataTest.java
+++ projeto-spring-data/src/test/java/projeto/spring/data/aula/AppStringDataTest.java
@@ -44,5 +44,22 @@ public class AppStringDataTest {
 		System.out.println(usuarioSpringData.get().getLogin());
 		System.out.println(usuarioSpringData.get().getSenha());
 	}
+	@Test
+	public void testeConsultaTodos() {
+		
+		Iterable<UsuarioSpringData> lista = interfaceSpringDataUser.findAll();
+		
+		for (UsuarioSpringData usuarioSpringData : lista) {
+			
+			System.out.println(usuarioSpringData.getEmail());
+			System.out.println(usuarioSpringData.getIdade());
+			System.out.println(usuarioSpringData.getLogin());
+			System.out.println(usuarioSpringData.getNome());
+			System.out.println(usuarioSpringData.getSenha());
+			System.out.println("--------------------------------------------");
+			
+		}
+		
+	}
 
 }
